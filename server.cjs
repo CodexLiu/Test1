cdecc4b73eacb34de63a3a9fd4abeb9b839f00f8
Cody Liu <124545714+CodexLiu@users.noreply.github.com>
2023-04-19 22:19:13 -0700

    Test comments

--- server.cjs
+++ server.cjs
@@ -55,16 +55,16 @@ app.post("/upload", upload.single("pdf"), (req, res) => {
         console.log("Deleted .DS_Store file");
 
         // Run the npm run ingest command
-        exec("npm run ingest", (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error executing command: ${error.message}`);
-            return;
-          }
-          if (stderr) {
-            console.error(`Command stderr: ${stderr}`);
-            return;
-          }
-          console.log(`Command stdout: ${stdout}`);
+        //exec("npm run ingest", (error, stdout, stderr) => {
+          //if (error) {
+            //console.error(`Error executing command: ${error.message}`);
+            //return;
+          //}
+          //if (stderr) {
+            //console.error(`Command stderr: ${stderr}`);
+            //return;
+          //}
+          //console.log(`Command stdout: ${stdout}`);
 
           // Delete the contents of the docs folder
           exec("rm -rf ./docs/*", (error, stdout, stderr) => {
