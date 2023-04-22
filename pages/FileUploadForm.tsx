6d33a00c60f37502e4a801137469920637e26b32
Cody Liu <124545714+CodexLiu@users.noreply.github.com>
2023-04-21 20:51:20 -0700

    Added Upload Status Bar

--- pages/FileUploadForm.tsx
+++ pages/FileUploadForm.tsx
@@ -1,10 +1,12 @@
 import React, { ChangeEvent, useCallback, useState } from 'react';
+import axios, { AxiosProgressEvent } from 'axios';
 
 interface FileUploadFormProps {}
 
 const FileUploadForm: React.FC<FileUploadFormProps> = () => {
   const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
   const [uploadStatus, setUploadStatus] = useState<string>('');
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
 
   const handleFileChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -18,22 +20,22 @@ const FileUploadForm: React.FC<FileUploadFormProps> = () => {
       if (selectedFiles) {
         const formData = new FormData();
         formData.append('pdf', selectedFiles[0]);
-
+  
         try {
-          const response = await fetch('http://35.90.17.67:4000/upload', {
-            method: 'POST',
-            body: formData,
+          await axios.post('http://localhost:4000/upload', formData, {
+            onUploadProgress: (progressEvent: AxiosProgressEvent) => {
+              if (progressEvent.total) {
+                const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+                setUploadProgress(progress);
+              }
+            },
           });
-
-          if (response.ok) {
-            setUploadStatus('PDF file(s) uploaded successfully!');
-          } else {
-            setUploadStatus('An error occurred while uploading the PDF file(s)');
-          }
+  
+          setUploadStatus('PDF file(s) uploaded successfully!');
         } catch (error) {
-          setUploadStatus('An error occurred while uploading the PDF file');
+          setUploadStatus('An error occurred while uploading the PDF file(s)');
         }
-
+  
         setSelectedFiles(null);
       }
     },
@@ -47,6 +49,14 @@ const FileUploadForm: React.FC<FileUploadFormProps> = () => {
     >
       <h3 className="text-lg font-semibold mb-2">Upload Documents to Digestive Database</h3>
       {uploadStatus && <p>{uploadStatus}</p>}
+      {uploadProgress > 0 && (
+        <div className="w-full h-2 bg-gray-300 rounded-md">
+          <div
+            className="h-2 bg-blue-600 rounded-md"
+            style={{ width: `${uploadProgress}%` }}
+          ></div>
+        </div>
+      )}
       <div className="flex gap-4 items-center">
         <input
           type="file"
